7f393fa2480148b14a037afecd4a95fe176ede29
al_b <al.b.other@gmail.com>
2017-01-24 21:42:50 +0100

    float define freq notes

--- projet/TP-info2/userLibraries/include/define_notes.h
+++ projet/TP-info2/userLibraries/include/define_notes.h
@@ -6,31 +6,35 @@
   
   	#define LA_440_FREQ 440
 	
-	#define DO_3_FREQ		(LA_440_FREQ * pow(2.00, ((-9)/12)))
-	#define REb_3_FREQ		(LA_440_FREQ * pow(2.00, ((-8)/12)))
-	#define RE_3_FREQ		(LA_440_FREQ * pow(2.00, ((-7)/12)))
-	#define MIb_3_FREQ		(LA_440_FREQ * pow(2.00, ((-6)/12)))
-	#define MI_3_FREQ		(LA_440_FREQ * pow(2.00, ((-5)/12)))
-	#define FA_3_FREQ		(LA_440_FREQ * pow(2.00, ((-4)/12)))
-	#define SOLb_3_FREQ		(LA_440_FREQ * pow(2.00, ((-3)/12)))
-	#define SOL_3_FREQ		(LA_440_FREQ * pow(2.00, ((-2)/12)))
-	#define LAb_3_FREQ		(LA_440_FREQ * pow(2.00, ((-1)/12)))
+	
+	
+	#define DO_3_FREQ		(LA_440_FREQ * pow(2.00, ((-9.00)/12.00)))
+	#define REb_3_FREQ		(LA_440_FREQ * pow(2.00, ((-8.00)/12.00)))
+	#define RE_3_FREQ		(LA_440_FREQ * pow(2.00, ((-7.00)/12.00)))
+	#define MIb_3_FREQ		(LA_440_FREQ * pow(2.00, ((-6.00)/12.00)))
+	#define MI_3_FREQ		(LA_440_FREQ * pow(2.00, ((-5.00)/12.00)))
+	#define FA_3_FREQ		(LA_440_FREQ * pow(2.00, ((-4.00)/12.00)))
+	#define SOLb_3_FREQ		(LA_440_FREQ * pow(2.00, ((-3.00)/12.00)))
+	#define SOL_3_FREQ		(LA_440_FREQ * pow(2.00, ((-2.00)/12.00)))
+	#define LAb_3_FREQ		(LA_440_FREQ * pow(2.00, ((-1.00)/12)))
 	#define LA_3_FREQ		LA_440_FREQ
-	#define SIb_3_FREQ		(LA_440_FREQ * pow(2.00, (1/12)))
-	#define SI_3_FREQ		(LA_440_FREQ * pow(2.00, (2/12)))
+	#define SIb_3_FREQ		(LA_440_FREQ * pow(2.00, (1.00/12.00)))
+	#define SI_3_FREQ		(LA_440_FREQ * pow(2.00, (2.00/12.00)))
+	
+	
 
-	#define DO(a)		(DO_3_FREQ		*	pow(2.00, ( (a) -3)))
-	#define REb(a)		(REb_3_FREQ		*	pow(2.00, ((a)-3)))
-	#define RE(a)		(RE_3_FREQ		*	pow(2.00, ((a)-3)))
-	#define MIb(a)		(MIb_3_FREQ		*	pow(2.00, ((a)-3)))
-	#define MI(a)		(MI_3_FREQ		*	pow(2.00, ((a)-3)))
-	#define FA(a)		(FA_3_FREQ		*	pow(2.00, ((a)-3)))
-	#define SOLb(a)		(SOLb_3_FREQ	*	pow(2.00, ((a)-3)))
-	#define SOL(a)		(SOL_3_FREQ		*	pow(2.00, ((a)-3)))
-	#define LAb(a)		(LAb_3_FREQ		*	pow(2.00, ((a)-3)))
-	#define LA(a)		(LA_3_FREQ		*	pow(2.00, ((a)-3)))
-	#define SIb(a)		(SIb_3_FREQ		*	pow(2.00, ((a)-3)))
-	#define SI(a)		(SI_3_FREQ		*	pow(2.00, ((a)-3)))
+	#define DO(a)		(DO_3_FREQ		*	pow(2.00, ((a)-3.00)))
+	#define REb(a)		(REb_3_FREQ		*	pow(2.00, ((a)-3.00)))
+	#define RE(a)		(RE_3_FREQ		*	pow(2.00, ((a)-3.00)))
+	#define MIb(a)		(MIb_3_FREQ		*	pow(2.00, ((a)-3.00)))
+	#define MI(a)		(MI_3_FREQ		*	pow(2.00, ((a)-3.00)))
+	#define FA(a)		(FA_3_FREQ		*	pow(2.00, ((a)-3.00)))
+	#define SOLb(a)		(SOLb_3_FREQ	*	pow(2.00, ((a)-3.00)))
+	#define SOL(a)		(SOL_3_FREQ		*	pow(2.00, ((a)-3.00)))
+	#define LAb(a)		(LAb_3_FREQ		*	pow(2.00, ((a)-3.00)))
+	#define LA(a)		(LA_3_FREQ		*	pow(2.00, ((a)-3.00)))
+	#define SIb(a)		(SIb_3_FREQ		*	pow(2.00, ((a)-3.00)))
+	#define SI(a)		(SI_3_FREQ		*	pow(2.00, ((a)-3.00)))
 	
 	
 	
@@ -45,6 +49,6 @@
 	#define NUM_LAb		9
 	#define NUM_LA		10
 	#define NUM_SIb		11
-  	#define NUM_SI	12
+  	#define NUM_SI		12
   
   #endif
