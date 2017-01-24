d624c293b52289883ae7f35d23c3e2b96e0736b9
albother <al.b.other@gmail.com>
2017-01-24 21:15:49 +0100

    commentaires

--- projet/TP-info2/userLibraries/include/display.h
+++ projet/TP-info2/userLibraries/include/display.h
@@ -8,10 +8,50 @@
 #ifndef __DISPLAY_H__
 #define __DISPLAY_H__
 
+
+/*
+* Overview : Affichage du titre du projet sur la première ligne de l'afficheur
+* Author : METAYER Simon
+* Params : none
+* Return : none
+*/
 void displayTitle();
+
+
+/*
+* Overview : Convertie un code représentant la note en un texte contenant le nom de la note
+* Author : BIZON Alexis
+* Params :  const char: note ->Le code de la note;
+            char*: str ->La chaine de caractères du nom de la note
+* Return : none
+*/
 void noteToString(const char note, char* str);
+
+
+/*
+* Overview : Affiche la fréquence envoyée en paramètre
+* Author : METAYER Simon
+* Params :  float freq ->La fréquence à afficher
+* Return : none
+*/
 void displayFrequence(float freq);
+
+
+/*
+* Overview : Affiche la note envoyée en paramètre
+* Author : BIZON Alexis
+* Params :  const char note ->La note à afficher
+* Return : none
+*/
 void displayNote(const char note);
+
+
+/*
+* Overview : Affiche le degré ou l'octave envoyée en paramètre
+* Author : BIZON Alexis
+* Params :  char degre ->Le degré à afficher
+* Return : none
+*/
 void displayDegre(char degre);
 
 
