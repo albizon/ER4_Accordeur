108c2149e8b3cc8eaed4d7baf6a534c45a1f3d7e
Alexis BIZON <al.b.other@gmail.com>
2017-01-25 22:05:54 +0100

    Update instruments.h

--- projet/TP-info2/userLibraries/include/instruments.h
+++ projet/TP-info2/userLibraries/include/instruments.h
@@ -1,31 +1,50 @@
 #ifndef __INSTRUMENTS_H__
 #define __INSTRUMENTS_H__
 
+
+#define DELAY 1
+#define ATTACK 2
+#define HOLD 3
+#define DECAY 4
+#define SUSTAIN 5
+#define RELEASE 6
+
 struct Instrument
 {
-	char name[50];
-	char codeMIDI;
-	float H0;
-	float H1;
-	float H2;
-	float H3;
-	float H4;
-	float H5;
-	float H6;
-	float H7;
-	float H8;
-	float H9;
-	float H10;
-	float H11;
-	float H12;
-	float H13;
-	float H14;
-	float H16;
-}
+	char name[50];//Nom de l'instrument
+	char isMIDIInstrument;//Dit si l'instrument fait parti de la norme MIDI
+	char codeMIDI;//Code MIDI de l'instrument
+	float H0 = 0;//Amplitude relative de la valeure moyenne du signal par rapport au fondamental en %
+	float H1 = 100;//Amplitude relative du fondamental du signal par rapport au fondamental en %
+	float H2;//Amplitude relative du second harmonique du signal par rapport au fondamental en %
+	float H3;//Amplitude relative du troisième harmonique du signal par rapport au fondamental en %
+	float H4;//Amplitude relative du quatrième harmonique du signal par rapport au fondamental en %
+	float H5;//Amplitude relative du cinquième harmonique du signal par rapport au fondamental en %
+	float H6;//Amplitude relative du sixième harmonique du signal par rapport au fondamental en %
+	float H7;//Amplitude relative du septième harmonique du signal par rapport au fondamental en %
+	float H8;//Amplitude relative du huitième harmonique du signal par rapport au fondamental en %
+	float H9;//Amplitude relative du neuvième harmonique du signal par rapport au fondamental en %
+	float H10;//Amplitude relative du dizième harmonique du signal par rapport au fondamental en %
+	float H11;//Amplitude relative du onzième harmonique du signal par rapport au fondamental en %
+	float H12;//Amplitude relative du douzième harmonique du signal par rapport au fondamental en %
+	float H13;//Amplitude relative du treizième harmonique du signal par rapport au fondamental en %
+	float H14;//Amplitude relative du quatorzième harmonique du signal par rapport au fondamental en %
+	float H15;//Amplitude relative du quinzième harmonique du signal par rapport au fondamental en %
+	float H16;//Amplitude relative du seizième harmonique du signal par rapport au fondamental en %
+	
+	float delay = 0;//durée entre le début de la note et le début de l'attaque en seconde
+	float attack;//durée de la montée de la note jusqu'à so maximum en seconde
+	float hold = 0;//durée où la note reste à son maximum en seconde
+	float decay;//durée de descente de la note aprés le maximum vers le maintient en seconde
+	float sustain;//amplitude relative de la note pendant sa pèriode de maintient par rapport au maximum en %
+	float release;//durée de descente de la note jusqu'à zero aprés relachement de la note en seconde
+};
 
-Instrument getInstru(float *env, Instrument *listInstru);
+struct Instrument getInstru(float *env, Instrument *listInstru);
 
-float getFreqPlay(float *env, Instrument instru);
+float getFreqPlay(float *env, struct Instrument instru);
+
+void getEnveloppe(float freqNote, struct Instrument instru, char mode, float *env);//à revoir
 
 
 #endif
