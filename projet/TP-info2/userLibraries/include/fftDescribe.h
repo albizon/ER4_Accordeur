0a10b1092f8955fb7d00ee2c80bf5ffdca72ec0a
simomMe <simonmetayer@neuf.fr>
2017-01-27 13:04:19 +0100

    Update fftDescribe.h

--- projet/TP-info2/userLibraries/include/fftDescribe.h
+++ projet/TP-info2/userLibraries/include/fftDescribe.h
@@ -1,1 +1,12 @@
+#ifndef __FFTDESCRIBE_H__
+#define __FFTDESCRIBE_H__
 
+#include <vector.h>
+
+void complexLinearToComplexEXponential(const vector<float> real, const vector<float> imag, vector<float> *mod, vector<float> *arg);
+
+void extractEnveloppes(const vector<float> mod, const vector<float> arg, vector<vector<float>> *envs)
+
+
+
+#endif
