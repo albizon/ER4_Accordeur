3ba50ede5b25c04f52b9149318d37fef04b36049
Alexis BIZON <al.b.other@gmail.com>
2017-01-29 18:14:05 +0100

    Update TP-info2.c

--- projet/TP-info2/TP-info2.c
+++ projet/TP-info2/TP-info2.c
@@ -14,7 +14,7 @@ int main(void)//fonction principale du programme
 	float freq = 49;//contient la fréquence mesurée de la note
 	char note = 0;//contient la note calculée à partir de la fréquence, voir les différentes valeurs dans define_notes.h
 	char degre = 0;//contient le degré, ou l'octave de la note calculée à partir de la fréqence
-	float plage = 1.0;//spécifie l'erreur relative autorisée en % entre la fréquence mesurée et la fréquence réelle correspondant à la note
+	float relativeError = 1.0;//spécifie l'erreur relative autorisée en % entre la fréquence mesurée et la fréquence réelle correspondant à la note
 	setup();//Fonction d'initialisation
 	displayTitle();//affiche le titre du projet en haut de l'écran OLED
 	while(1)
@@ -22,7 +22,7 @@ int main(void)//fonction principale du programme
 		//freq=getFreq();//Récupère la fréquence mesurée par le fréquencemètre
 		note = noteSolver(freq);//permet de déterminer la note correspondant à la fréquence mesurée
 		degre = degreSolver(freq);//permet de déterminer le degré ou l'octave correspondant à la fréquence mesurée
-		char acc = accorder(note, degre, freq, calculAbsError(plage, freq));//Permet de déterminer si la note est acoordée, trop basse ou trop haute
+		char acc = accorder(note, degre, freq, calculAbsError(relativeError, freq));//Permet de déterminer si la note est acoordée, trop basse ou trop haute
 		diplayLedIndicator(acc); // Allume les leds en fonciton de l'indicateur : trop bas / ok / trop haut
 		displayFrequence(freq);//On affiche la fréquence sur l'afficheur OLED
 		displayNote(note);//On affiche la note sur l'afficheur OLED
