0fba6c05aad21e667275916a6af9254614b4f925
Alexis BIZON <al.b.other@gmail.com>
2017-01-30 09:13:14 +0100

    commentaires define.h

--- projet/TP-info2/userLibraries/include/define.h
+++ projet/TP-info2/userLibraries/include/define.h
@@ -22,11 +22,14 @@
 	#include "define_notes.h"
 	#include <stdlib.h>
 	
-	#define LOW_FREQ	1
-	#define OK_FREQ		2
-	#define HIGH_FREQ	3
+	#define LOW_FREQ	1 //La fréquence est trop basse
+	#define OK_FREQ		2 //La fréquence est OK
+	#define HIGH_FREQ	3 //La fréquence est trop haute
 
-	#define POURCENT_TO_SCALAIRE /100
-	#define SCALAIRE_TO_POURCENT *100
+	#define POURCENT_TO_SCALAIRE /100 //Permet de convertir un nombre en % vers un nombre scalaire
+	#define SCALAIRE_TO_POURCENT *100 //Permet de convertir un nombre en scalaire vers un nombre en %
+
+	#define TRUE 1 //Vrai/Oui
+	#define FALSE 0 //Faux/Non
 	
 #endif
