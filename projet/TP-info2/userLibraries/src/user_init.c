ad3dcd56431a188b8959c2a08d7ddc3aa4ae2e79
simomMe <simonmetayer@neuf.fr>
2017-01-30 08:43:52 +0100

    commentaire userinit.c

--- projet/TP-info2/userLibraries/src/user_init.c
+++ projet/TP-info2/userLibraries/src/user_init.c
@@ -4,12 +4,13 @@
 
 void setup()//Fonction d'initialisation
 {
-	SystemInit();//Initialisation des param�tres syst�mes
+	SystemInit();//Initialisation des paramètres systèmes
 	redirectPrintf(PRINTF_OLED);//On redirige les instructions de la fonction printf sur l'afficheur OLED
 	OLEDInit(1);//On initialise notre afficheur OLED avec aucune image
 	ADCInit(); // initialisation de l'adc
-	initFreq();
-	pinMode(PORTA, 12, OUTPUT);
+	initFreq(); // initialisation du fréquencemètre
+	// Initialisation des leds d'aide à l'accordage :
+	pinMode(PORTA, 12, OUTPUT); 
 	pinMode(PORTA, 13, OUTPUT);
 	pinMode(PORTA, 15, OUTPUT);
 }
