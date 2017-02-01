8c6f4d9a44867f7d5148625ea16114e22affb023
simomMe <simonmetayer@neuf.fr>
2017-02-01 09:33:02 +0100

    del fft

--- projet/TP-info2/userLibraries/src/fft.c
+++ projet/TP-info2/userLibraries/src/fft.c
@@ -18,107 +18,41 @@ floatSingleArray *_imag;
 int _deltaT;
 int _deltaF;
 int _size;
-	
-void fft(int inverse, floatSingleArray *R, floatSingleArray *I)
-{
-	int N=0;
-	if(sizeof(&R)>sizeof(&I)) {N = sizeof(I);}
-	else {N = sizeof(R);}
-	N/=sizeo(*R);
-  const float PI = 3.141592 ;
-  const float UFL = 1.0E-38 ;
-  const float OVF = 1.0E38 ;
-  int N1,N2,J,K,M,L1,L2,L,I5,IJ;
-  float T1,T2,W1,W2,U1,U2,Z1,Z2;
-  
-  if (inverse==TRUE)
-  {
-      for (IJ=0; IJ<(N-1);IJ++)
-      {
-        R[IJ] = R[IJ]/N;
-		I[IJ] = I[IJ]/N;
-      }
-  }
-  
-  M = arrondi(log(N)/log(2)) ;
-	N2 = N / 2 ;
-	N1 = N-1 ;
-	J = 1 ;
-  
-  for (IJ=0; (IJ<N1) ; IJ++)
-  {
-  	if (IJ<J)
-         {
-		T1 = R[J-1] ;
-		T2 = I[J-1] ;
-		R[J-1] = R[IJ-1] ;
-		I[J-1] = I[IJ-1] ;
-		R[IJ-1] = T1 ;
-		I[IJ-1] = T2 ;
-         }
-         K = N2 ;
-         do {
-			J = J - K ;
-			K =K / 2 ;
-         } while (K<J);
-         J = J+K ;
-  }
-  
-  for  (L=1; L<M; L++ )
-  {
-	L1 = floor(exp(L*log(2))) ;
-	L2 = L1 / 2 ;
-	U1 = 1 ;
-	U2 = 0 ;
-	W1 = cos(PI/L2) ;
-	W2 = 0-sin(PI/L2) ;
-	if (inverse==TRUE)  W2 = 0-W2 ;
-	for (J=1; J<L2; J++ )
-	{
-		IJ = J ;
-		do{
-                   I5 = IJ+L2 ;
-                   T1 = R[I5-1]*U1-I[I5-1]*U2 ;
-                   T2 = R[I5-1]*U2+I[I5-1]*U1 ;
-                   R[I5-1] = R[IJ-1] - T1 ;
-                   I[I5-1] = I[IJ-1] - T2 ;
-                   R[IJ-1] = R[IJ-1] + T1 ;
-                   I[IJ-1] = I[IJ-1] + T2 ;
-                   IJ = IJ + L1 ;
-                } while( IJ>=N) ;
-             
-                Z1 =U1*W1-U2*W2 ;
-             
-                if (abs(Z1)>OVF)
-                {
-					if (Z1>0) Z1 = OVF;
-					else Z1 = 0-OVF ;
-                }
-             
-                if (abs(Z1)<UFL)
-                {
-					Z1 = 0 ;
-					Z2 = U1*W2+U2*W1 ;
-                }
-             
-		if (abs(Z2)>OVF) 
-		{
-			if (Z2>0) Z2 = OVF;
-			else Z2 = 0-OVF ;
-		}
-             
-		if (abs(Z2)<UFL) 
-		{
-			Z1 = 0 ;
-			U1 = Z1 ;
-		}
-             
-		U2 = Z2 ;
-	}
-  }  
-  
-}
 
+void fft(int inverse, floatSingleArray *real, floatSingleArray *imag)
+//void FFT( complex double *x, , int N ) 
+{
+    
+tabSize=sizeof(real); // On récupère la taille du tableau
+floatSingleArray *temp_real; 
+temp_real = realloc(temp_real,tabSize);
+	
+	if (inverse == FALSE) // Si on veut faire une FFT
+	{
+		
+		
+		
+		/*for ( int k = 0; k < tabSize; k++ ) 
+		{
+			temp_real-> array[k] = 0;
+			
+			for ( int j = 0; j < tabSize; j++ ) 
+			{
+				temp_real-> array[k] += real[j] ;
+				imag     -> array[k] += cexp( 2.0 * I * M_PI * j * k / tabSize );
+			}*/
+	    }
+	    for ( int k = 0; k < tabSize; k++ ) 
+	    {
+		   real-> array[k] =  temp_real[k];
+	    }
+	}
+	else if (inverse == TRUE) // si on veut faire une FFT inverse
+	{
+		// ALGO FFT INVERSE
+	}
+	free( temp_real );
+}
 
 void adcFFTInit(int deltaT) // deltaT en us
 {
