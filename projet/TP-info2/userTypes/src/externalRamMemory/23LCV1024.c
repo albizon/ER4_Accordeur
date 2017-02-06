e0e7d65aba44b922c35e137f69fac83f3f7fef87
Alexis BIZON <al.b.other@gmail.com>
2017-02-06 09:49:07 +0100

    Update 23LCV1024.c

--- projet/TP-info2/userTypes/src/externalRamMemory/23LCV1024.c
+++ projet/TP-info2/userTypes/src/externalRamMemory/23LCV1024.c
@@ -1,1 +1,27 @@
 #include "23LCV1024.h"
+
+void initCircuitRam(void)
+{
+  
+}
+
+
+void writeExternalRam_uint32(uint32_t address, uint32_t val)
+{
+  
+}
+
+uint32_t readExternalRam_uint32(uint32_t address)
+{
+  
+}
+
+uint32_t allocExternalRam(uint32_t size)
+{
+  
+}
+
+void freeExternalRam(uint32_t address, uint32_t size)
+{
+  
+}
